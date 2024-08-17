9c055e99884639fb7fb19efc6159634564ca72bf
KHUSHI 444 <khankhushi4452@gmail.com>
2024-08-17 22:07:40 +0530

    basic changes

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
