adaac38abbcc5a8f91b2f6afcf1399675a0f82ae
KHUSHI 444 <khankhushi4452@gmail.com>
2024-08-17 22:11:12 +0530

    dotenv setup

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 const express = require('express');
+import dotenv from "dotenv";
 
 
+dotenv.config();
 const app = express();
 
 
