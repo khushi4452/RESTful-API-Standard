a76190fcfd6a487563120e290148caffc104b377
KHUSHI 444 <khankhushi4452@gmail.com>
2024-08-17 22:18:11 +0530

    basic setup of MongoDb

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ import express from 'express';
 import mongoose from 'mongoose';
 const express = require('express');
 import dotenv from "dotenv";
-
-
 dotenv.config();
+
 const app = express();
 
+app.use(express.json());
+
+const connDB = async ()=>{
+  try {
+   const conn = await mongoose.connect(process.env.MONGO_DB_URI);
+
+   if(conn) {
+     console.log("MongoDb connected successfully");
+  }
+  
+  }catch(error){
+  console.error("Error connecting to MongoDB:", error.message);
+  }
+  };
+  connDB();
+      
+   
 
 const PORT = process.env.PORT || 3000;
 
