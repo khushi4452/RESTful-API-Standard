d6a5c37a65876434921b34b81ea1d8a121b1c7f5
KHUSHI 444 <khankhushi4452@gmail.com>
2024-08-19 20:55:43 +0530

    Mongodb  url connect

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 import express from 'express';
 import mongoose from 'mongoose';
-const express = require('express');
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-const connDB = async ()=>{
+const connDB = async () => {
   try {
-   const conn = await mongoose.connect(process.env.MONGO_DB_URI);
+    const conn = await mongoose.connect(process.env.MONGO_DB_URI);
 
-   if(conn) {
-     console.log("MongoDb connected successfully");
+    if (conn) {
+      console.log("MongoDB connected successfully👌");
+    }
+
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
   }
-  
-  }catch(error){
-  console.error("Error connecting to MongoDB:", error.message);
-  }
-  };
-  connDB();
-      
-   
+};
+
+connDB();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`server listening on ${PORT}`);
 });
