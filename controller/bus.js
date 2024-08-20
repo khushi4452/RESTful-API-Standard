63958ec5b59bfa419b2a8007969e67d82c1a7973
KHUSHI 444 <khankhushi4452@gmail.com>
2024-08-20 22:00:19 +0530

    created endpoint for bus

--- controller/bus.js
+++ controller/bus.js
@@ -1,1 +1,22 @@
-import Bus from "../models/Bus.js"
+import Bus from "../models/Bus.js";
+const postApiv1Busses = async (req , res) =>{
+    const {busNo , busType , seatType } = req.body;
+   const bus = new bus ({
+    busNo ,
+    busType ,
+    seatType,
+   });
+ try {
+    const SavedBus = await bus.save();
+    res.status(200).json({
+        success : true,
+        data: SavedBus,
+        message: "Bus saved successfully",
+    });
+ }  catch (error) {
+    res.status(422).json({
+        success: false,
+        message: error.message,
+    })
+ }
+};
