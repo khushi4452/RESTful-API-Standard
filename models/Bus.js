2bbdd1ee3cc2985c9ca8589ef29009aff792c3d0
KHUSHI 444 <khankhushi4452@gmail.com>
2024-08-20 22:03:46 +0530

    basic model of bus

--- models/Bus.js
+++ models/Bus.js
@@ -1,4 +1,10 @@
 import { Schema, model } from "mongoose";
-const busSchema = new Schema ({
-  
-})
+const busSchema = new Schema (
+    {
+        busNo:{
+            type: String,
+            required: true,
+            enum : ["111","654","786","999"],
+        },
+    }
+)
