1512e4b0b294cc7967462425380a374b232b8537
KHUSHI 444 <khankhushi4452@gmail.com>
2024-08-21 18:20:00 +0530

    controller created

--- controller/bus.js
+++ controller/bus.js
@@ -17,6 +17,37 @@ const postApiv1Busses = async (req , res) =>{
     res.status(422).json({
         success: false,
         message: error.message,
-    })
+    });
  }
 };
+
+const postApiv2Busses = aync (req,res)=>{
+    const { busNo,
+      busType,
+      seatType,
+      totalSeatCount ,} = req.body;
+
+      const bus = new Bus ({
+      busNo,
+      busType,
+      seatType,
+      totalSeatCount,
+        });
+
+  try {
+    const savedBus = await bus.save();
+    res.status(200).json({
+    success: true,
+    data: savedBus,
+    message: "Bus saved successfully",
+ });
+  } catch (error) {
+    res.status(422).json({
+    success: false,
+     message: error.message,
+ });
+ }
+        
+};
+ export { postApiv1Busses, postApiv2Busses }
+
