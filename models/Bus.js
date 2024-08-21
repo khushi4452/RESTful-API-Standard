9c469cd2868d7a7d542d7c9d82f1b7b5a5300646
KHUSHI 444 <khankhushi4452@gmail.com>
2024-08-21 18:13:18 +0530

    bus Schema Ready

--- models/Bus.js
+++ models/Bus.js
@@ -1,10 +1,26 @@
 import { Schema, model } from "mongoose";
 const busSchema = new Schema (
     {
-        busNo:{
-            type: String,
-            required: true,
-            enum : ["111","654","786","999"],
-        },
-    }
-)
+   busNo:{
+   type: String,
+   required: true,
+   enum : ["111","654","786","999"],
+   },
+   seatType:{
+    type: String,
+    enum:["seater", "sleeper"],
+   },
+   busType: {
+    type: String,
+    enum : ["ac","nonac"],
+   },
+   totalSeatCount: {
+    type: Number,
+   },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Bus = model("Bus", busSchema);
+export default Bus;
